30ffd59297ee0c58b83f60df6bafc05bb8bd566d
JunyTM <20010784@st.phenikaa-uni.edu.com>
2023-03-12 18:20:01 +0700

    update view home screen & create profile screen

--- src/views/administer/Home.js
+++ src/views/administer/Home.js
@@ -2,9 +2,10 @@ import React from 'react'
 import '../../assets/styles/dashboard.css'
 import Header from '../../components/layout/Header'
 import Navigate from '../../components/layout/Navigate'
-import RenderLineChart from '../../components/chart'
+// import RenderLineChart from '../../components/chart'
+import PriceList from '../../components/layout/PriceList'
 import TimeLine from '../../components/chart/TimeLine'
-import { DollarOutlined, AliwangwangOutlined, DotChartOutlined } from '@ant-design/icons'
+import { AliwangwangOutlined, DotChartOutlined, BulbOutlined, UngroupOutlined } from '@ant-design/icons'
 
 
 import { Layout } from 'antd'
@@ -20,39 +21,40 @@ export default function Home() {
       <div className='row-span-5 col-span-5'>
         <div className='w-[96%] h-[30%] ml-5 mb-10 pt-4 flex justify-around bg-white shadow-lg shadow-gray-200'>
           <div className='w-64 h-24  bg-slate-200 rounded-md flex'>
-            <DollarOutlined style={{ fontSize: '55px', color: '#6666FF' }} className=' m-5 bg-gray-300 rounded-full' />
+            <AliwangwangOutlined style={{ fontSize: '55px', color: '#6666FF' }} className=' m-5 bg-gray-300 rounded-full' />
             <div className='flex flex-col items-end mt-3'>
-              <p className='font-semibold'>Hạng thành viên</p>
-              <h1 className='text-4xl'>Bạc</h1>
+              <p className='font-semibold'>Dịch vụ thông tin</p>
+              <h1 className='text-4xl'>tin tức</h1>
             </div>
           </div>
 
           <div className='w-64 h-24  bg-slate-200 rounded-md flex'>
             <DotChartOutlined style={{ fontSize: '55px', color: '#6666FF' }} className='m-5 bg-gray-300 rounded-full' />
             <div className='flex flex-col items-end mt-3 ml-3'>
-              <p className='font-semibold'>Tích điểm</p>
-              <h1 className='text-4xl'>320</h1>
+              <p className='font-semibold'>Đăng ký dịch vụ</p>
+              <h1 className='text-4xl'>online</h1>
             </div>
           </div>
 
           <div className='w-72 h-24  bg-slate-200 rounded-md flex'>
-            <AliwangwangOutlined style={{ fontSize: '55px', color: '#6666FF' }} className='m-5 bg-gray-300 rounded-full' />
+            <UngroupOutlined style={{ fontSize: '55px', color: '#6666FF' }} className='m-5 bg-gray-300 rounded-full' />
             <div className='flex flex-col items-end mt-3 ml-2'>
-              <p className='font-semibold'>Giá tiền điện</p>
-              <h1 className='text-4xl'>3.000 đ/số</h1>
+              <p className='font-semibold'>Cơ sở hạ tầng</p>
+              <h1 className='text-4xl'>khu vực</h1>
             </div>
           </div>
 
-          <div className='w-80 h-24  bg-slate-200 rounded-md flex'>
-            <AliwangwangOutlined style={{ fontSize: '55px', color: '#6666FF' }} className='m-5 bg-gray-300 rounded-full' />
+          <div className='w-64 h-24  bg-slate-200 rounded-md flex'>
+            <BulbOutlined style={{ fontSize: '55px', color: '#6666FF' }} className='m-5 bg-gray-300 rounded-full' />
             <div className='flex flex-col items-end mt-3 '>
-              <p className='font-semibold'>Mã người dùng</p>
-              <h1 className='text-4xl'>1121436013</h1>
+              <p className='font-semibold'>Tra cứu dịch vụ</p>
+              <h1 className='text-4xl'>hỗ trợ</h1>
             </div>
           </div>
         </div>
         <div className='w-full flex'>
-          <RenderLineChart />
+          {/* <RenderLineChart /> */}
+          <PriceList />
           <TimeLine />
         </div>
       </div>
--- src/views/profile/Profile.js
+++ src/views/profile/Profile.js
@@ -1,7 +1,61 @@
 import React from 'react'
+import '../../assets/styles/dashboard.css'
+import Header from '../../components/layout/Header'
+import Navigate from '../../components/layout/Navigate'
+
+
+import { Layout, Avatar, Space } from 'antd'
+const { Sider } = Layout
 
 export default function Profile() {
+  
   return (
-    <div>Profile</div>
+    <div className="Container grid grid-rows-6 grid-cols-6 grid-flow-col gap-2">
+      <Sider className='bg-green-100 row-span-6 rm_max_width'>
+        <Navigate />
+      </Sider>
+      <Header />
+      <div className='row-span-5 col-span-5'>
+        <div className='w-[90%] h-[90%] flex bg-white m-auto border-t-8 border-sky-200'>
+          <div className='w-80 h-1/2 m-6 border-r-2 border-gray-200'>
+            <Avatar size={200} className='ml-[20%]' icon={<img src='https://scontent.xx.fbcdn.net/v/t1.15752-9/335922011_884245539328409_8181192386167189152_n.png?stp=dst-png_s206x206&_nc_cat=103&ccb=1-7&_nc_sid=aee45a&_nc_ohc=Hlaqo6g7s44AX9WnPAu&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQkg2LpUL7hppE14bdtGZUeM4s0fwwIbGRL-MM0s-OaCQ&oe=643519D0'/>} />
+              <h2 className='ml-16 mt-10 font-medium text-3xl'>Phạm Dức Hòa</h2>
+          </div>
+          <div className='w-3/4 h-1/2 mt-6 py-6 pl-4 bg-cyan-50'>
+            {/* setup form by table */}
+            <table className='w-full h-full'>
+              <tr>
+                <td className='w-36 font-medium'>Họ và tên</td>
+                <td className=''>Phạm Dức Hòa</td>
+              </tr>
+              <tr>
+                <td className='w-36 font-medium'>Ngày sinh</td>
+                <td className=''>15/3/2002</td>
+              </tr>
+              <tr>
+                <td className='w-36 font-medium'>Giới tính</td>
+                <td className=''>Nam</td>
+              </tr>
+              <tr>
+                <td className='w-36 font-medium'>CMND/CCCD</td>
+                <td className=''>78495335022</td>
+              </tr>
+              <tr>
+                <td className='w-36 font-medium'>Số điện thoại</td>
+                <td className=''>0123456789</td>
+              </tr>
+              <tr>
+                <td className='w-36 font-medium'>Email</td>
+                <td className=''>Hoabaosdom@gmail.com</td>
+              </tr>
+            </table>
+          </div>
+        </div> 
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
